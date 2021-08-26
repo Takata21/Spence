207c618960ca7b42db08f6f2e67d393060403bcc
Manuel Takata <takata211011@gmail.com>
2021-08-25 23:10:57 -0500

    fixing header bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-console.log("hey");
+const HeroImg = document.querySelector(".hero-img");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("hy");
+});
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 480) {
+        HeroImg.setAttribute("src", "./static/img/hero-img-tablet.png");
+    }
+    if (window.innerWidth > 1024) {
+        HeroImg.setAttribute("src", "./static/img/hero-img-desktop.png");
+    }
+});
