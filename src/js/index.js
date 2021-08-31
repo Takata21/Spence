7acaa7388972eecfed353d7d68fa528a1dd8825e
Manuel Takata <takata211011@gmail.com>
2021-08-31 00:15:06 -0500

    mockup and text editor section done

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 const HeroImg = document.querySelector(".hero-img");
+const mockupImg = document.querySelector(".escrow-img");
 document.addEventListener("DOMContentLoaded", () => {
     console.log("hy");
 });
 window.addEventListener("resize", () => {
     if (window.innerWidth > 480) {
         HeroImg.setAttribute("src", "./static/img/hero-img-tablet.png");
+        mockupImg.setAttribute("src", "./static/img/PhoneMockupTablet.png");
     }
     if (window.innerWidth > 1024) {
         HeroImg.setAttribute("src", "./static/img/hero-img-desktop.png");
+        mockupImg.setAttribute("src", "./static/img/PhoneMockup.png");
     }
 });
