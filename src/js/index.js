037570c7d29d0da7c15a635c58f776051b6bb019
Manuel Takata <takata211011@gmail.com>
2021-08-31 01:49:17 -0500

    footer section and top banner done

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 const HeroImg = document.querySelector(".hero-img");
 const mockupImg = document.querySelector(".escrow-img");
+const welcomeBanner = document.querySelector(".welcome-banner");
 document.addEventListener("DOMContentLoaded", () => {
+    welcomeBanner.classList.add("active");
+    setTimeout(() => {
+        welcomeBanner.classList.remove("active");
+    }, 4000);
     console.log("hy");
 });
 window.addEventListener("resize", () => {
